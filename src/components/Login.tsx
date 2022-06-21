88511706846b5aed31f8d2087dbafa9c723dbea3
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-06-21 23:17:46 +0430

    Handle States

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { User } from "models/Models";
+import React, { ChangeEvent, FormEvent, SyntheticEvent } from "react";
 import AuthService from "services/AuthService";
 
 interface LoginProps {
@@ -12,6 +13,10 @@ interface LoginState {
   loginSuccessful: boolean;
 }
 
+interface CustomEvent {
+  target: HTMLInputElement;
+}
+
 export default class Login extends React.Component<LoginProps, LoginState> {
   state: Readonly<LoginState> = {
     userName: "",
@@ -20,16 +25,39 @@ export default class Login extends React.Component<LoginProps, LoginState> {
     loginSuccessful: false,
   };
 
+  private setUserName(event: CustomEvent): void {
+    this.setState({ userName: event.target.value });
+  }
+
+  private setPassword(event: CustomEvent): void {
+    this.setState({ password: event.target.value });
+  }
+
+  private async handleSubmit(event: SyntheticEvent): Promise<void> {
+    event.preventDefault();
+    const result: User | undefined = await this.props.authService.login(this.state.userName, this.state.password);
+  }
+
   render(): React.ReactNode {
     return (
       <>
         <div>
-          <h2>Please Login!</h2>
+          <h2>Please Login! (user:1234)</h2>
           <br />
-          <form action="">
-            <input type="text" value={this.state.userName} placeholder="UserName" />
+          <form onSubmit={(e: FormEvent<HTMLFormElement>): Promise<void> => this.handleSubmit(e)}>
+            <input
+              type="text"
+              value={this.state.userName}
+              placeholder="UserName"
+              onChange={(e: ChangeEvent<HTMLInputElement>): void => this.setUserName(e)}
+            />
             <br />
-            <input type="password" value={this.state.password} placeholder="Password" />
+            <input
+              type="password"
+              value={this.state.password}
+              placeholder="Password"
+              onChange={(e: ChangeEvent<HTMLInputElement>): void => this.setPassword(e)}
+            />
             <br />
             <input type="submit" value="login" />
           </form>
