24c373c100c61b5046c21d394293f56f6c4d93f6
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-06-24 23:07:31 +0430

    Add spaces route

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,11 +3,13 @@ import Home from "components/Home";
 import Login from "components/Login";
 import Navbar from "components/Navbar";
 import Profile from "components/Profile";
+import Spaces from "components/Spaces";
 
 import { User } from "models/Models";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import AuthService from "services/AuthService";
+import DataService from "services/DataService";
 
 export default class App extends React.Component<{}, { user: User | undefined }> {
   constructor(props: any) {
@@ -19,6 +21,8 @@ export default class App extends React.Component<{}, { user: User | undefined }>
   }
 
   private authService: AuthService = new AuthService();
+  private dataService: DataService = new DataService();
+
   private setUser(user: User): void {
     this.setState({ user });
   }
@@ -31,6 +35,7 @@ export default class App extends React.Component<{}, { user: User | undefined }>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login authService={this.authService} setUser={this.setUser} />} />
           <Route path="/profile" element={<Profile authService={this.authService} user={this.state.user} />} />
+          <Route path="/spaces" element={<Spaces dataService={this.dataService} />} />
         </Routes>
       </main>
     );
