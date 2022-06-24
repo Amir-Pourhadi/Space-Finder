6e7891976d3bdf945414352bf33f626e20f87bbb
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-06-24 18:15:39 +0430

    Declare getUserAttrs

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,9 +1,18 @@
-import { User } from "models/Models";
+import { User, UserAttribute } from "models/Models";
 
 export default class AuthService {
   public async login(userName: string, password: string): Promise<User | undefined> {
     if (userName === "user" && password === "1234") {
       return { userName, email: "example@gmail.com" };
     } else return undefined;
+  }
+
+  public async getUserAttributes(user: User): Promise<UserAttribute[]> {
+    return [
+      { name: "Description", value: "Best user ever!" },
+      { name: "Job", value: "Engineer" },
+      { name: "Age", value: "25" },
+      { name: "Experience", value: "3 years" },
+    ];
   }
 }
