c4e1b04c6142c56ee3f8e8f7aad386e13c74c589
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-06-24 17:47:38 +0430

    Profile when user not logged in

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,10 +1,29 @@
+import { User, UserAttribute } from "models/Models";
 import React from "react";
+import { Link } from "react-router-dom";
+import AuthService from "services/AuthService";
 
-export default class Profile extends React.Component {
+interface ProfileState {
+  userAttributes: UserAttribute[];
+}
+
+interface ProfileProps {
+  user: User | undefined;
+  authService: AuthService;
+}
+
+export default class Profile extends React.Component<ProfileProps, ProfileState> {
   render(): React.ReactNode {
     return (
       <>
         <div>Welcome to the Profile Page!</div>
+        {this.props.user ? (
+          <h3>Hello {this.props.user.userName}</h3>
+        ) : (
+          <div>
+            Please <Link to="/login">Login</Link>
+          </div>
+        )}
       </>
     );
   }
