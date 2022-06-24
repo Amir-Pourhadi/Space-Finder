5d6b786742c2042f32eb2173848372f8769fc83c
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-06-24 11:46:59 +0430

    Add navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,16 @@
 import { User } from "models/Models";
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default class Navbar extends React.Component<{ user: User }> {
+export default class Navbar extends React.Component<{ user: User | undefined }> {
   render(): React.ReactNode {
     return (
       <>
-        <nav></nav>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/profile">Profile</Link>
+          {this.props.user ? <Link to="/logout">{this.props.user.userName}</Link> : <Link to="login">Login</Link>}
+        </nav>
       </>
     );
   }
