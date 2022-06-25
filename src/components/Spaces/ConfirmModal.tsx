36b65960903b0c9e4a07fb89fb3c7e80d69e9889
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-06-25 17:48:47 +0430

    Develope base nodes

--- src/components/Spaces/ConfirmModal.tsx
+++ src/components/Spaces/ConfirmModal.tsx
@@ -1,3 +1,23 @@
 import React from "react";
 
-export default class ConfirmModal extends React.Component {}
+interface ConfirmModalProps {
+  visible: boolean;
+  content: string;
+  close: () => void;
+}
+
+export default class ConfirmModal extends React.Component<ConfirmModalProps> {
+  render(): React.ReactNode {
+    if (!this.props.visible) return null;
+    else
+      return (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>Reservation Result...</h2>
+            <h3>{this.props.content}</h3>
+            <button onClick={this.props.close}>Got it</button>
+          </div>
+        </div>
+      );
+  }
+}
