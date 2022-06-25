055ddfa866d96137c09abad8708183f9a44b808e
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-06-26 00:24:24 +0430

    Login/Form Test

--- test/components/Login.test.tsx
+++ test/components/Login.test.tsx
@@ -8,8 +8,30 @@ describe("Login Component Test Suit", () => {
   test("Heading Test", () => {
     render(<Login authService={authServiceMock as any} setUser={setUserMock} />);
 
-    const title: HTMLElement = screen.getByRole("heading");
-    expect(title).toBeTruthy();
-    expect(title).toHaveTextContent("Please Login! (user:1234)");
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    expect(heading).toHaveTextContent("Please Login");
+  });
+
+  test("Form Test", () => {
+    render(<Login authService={authServiceMock as any} setUser={setUserMock} />);
+
+    const userNameInput = screen.getByPlaceholderText(/username/i);
+    expect(userNameInput).toBeInTheDocument();
+    expect(userNameInput).toHaveValue("");
+
+    const passwordInput = screen.getByPlaceholderText(/password/i);
+    expect(passwordInput).toBeInTheDocument();
+    expect(passwordInput).toHaveValue("");
+
+    const loginBtn = screen.getByRole("button", { name: /login/i });
+    expect(loginBtn).toBeInTheDocument();
+    expect(loginBtn).toHaveValue("Login");
+
+    const failedLabel = screen.queryByText(/Login Failed/i);
+    expect(failedLabel).not.toBeInTheDocument();
+
+    const successLabel = screen.queryByText(/Login Successful/i);
+    expect(successLabel).not.toBeInTheDocument();
   });
 });
