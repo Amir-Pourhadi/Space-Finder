73b512fa356303c24a16fe66c1643afd9167e2a6
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-07-18 18:39:43 +0430

    Tests for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,12 +8,26 @@ export default class Navbar extends React.Component<{ user: User | undefined }> 
       <>
         <nav>
           <div>
-            <Link to="/">Home</Link>
-            <Link to="/profile">Profile</Link>
-            <Link to="/spaces">Spaces</Link>
+            <Link to="/" data-testid="home-link">
+              Home
+            </Link>
+            <Link to="/profile" data-testid="profile-link">
+              Profile
+            </Link>
+            <Link to="/spaces" data-testid="spaces-link">
+              Spaces
+            </Link>
           </div>
           <h1>Website</h1>
-          {this.props.user ? <Link to="/logout">{this.props.user.userName}</Link> : <Link to="login">Login</Link>}
+          {this.props.user ? (
+            <Link to="/logout" data-testid="logout-link">
+              {this.props.user.userName}
+            </Link>
+          ) : (
+            <Link to="/login" data-testid="login-link">
+              Login
+            </Link>
+          )}
         </nav>
       </>
     );
---
+++ test/components/Navbar.test.tsx
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/react";
+import Navbar from "components/Navbar";
+import { BrowserRouter } from "react-router-dom";
+import { correctUserInfo } from "../data/userData";
+
+const developmentUrl = "http://localhost";
+
+describe("Navbar Test Suit", () => {
+  test("Renders Correctly with user login", () => {
+    render(<Navbar user={undefined} />, { wrapper: BrowserRouter });
+
+    const homeLink: HTMLAnchorElement = screen.getByTestId("home-link");
+    expect(homeLink).toHaveTextContent("Home");
+    expect(homeLink.href).toEqual(developmentUrl + "/");
+
+    const profileLink: HTMLAnchorElement = screen.getByTestId("profile-link");
+    expect(profileLink).toHaveTextContent("Profile");
+    expect(profileLink.href).toEqual(developmentUrl + "/profile");
+
+    const spacesLink: HTMLAnchorElement = screen.getByTestId("spaces-link");
+    expect(spacesLink).toHaveTextContent("Spaces");
+    expect(spacesLink.href).toEqual(developmentUrl + "/spaces");
+
+    const loginLink: HTMLAnchorElement = screen.getByTestId("login-link");
+    expect(loginLink).toHaveTextContent("Login");
+    expect(loginLink.href).toEqual(developmentUrl + "/login");
+  });
+
+  test("Renders Correctly without user login", () => {
+    render(<Navbar user={correctUserInfo} />, { wrapper: BrowserRouter });
+
+    const homeLink: HTMLAnchorElement = screen.getByTestId("home-link");
+    expect(homeLink).toHaveTextContent("Home");
+    expect(homeLink.href).toEqual(developmentUrl + "/");
+
+    const profileLink: HTMLAnchorElement = screen.getByTestId("profile-link");
+    expect(profileLink).toHaveTextContent("Profile");
+    expect(profileLink.href).toEqual(developmentUrl + "/profile");
+
+    const spacesLink: HTMLAnchorElement = screen.getByTestId("spaces-link");
+    expect(spacesLink).toHaveTextContent("Spaces");
+    expect(spacesLink.href).toEqual(developmentUrl + "/spaces");
+
+    const logoutLink: HTMLAnchorElement = screen.getByTestId("logout-link");
+    expect(logoutLink).toHaveTextContent(correctUserInfo.userName);
+    expect(logoutLink.href).toEqual(developmentUrl + "/logout");
+  });
+});
