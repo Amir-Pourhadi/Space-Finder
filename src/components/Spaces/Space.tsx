983e095d2d1feb31f26780f3a9b20f75394bbf00
Amir Pourhadi <AmirPourhadi@outlook.com>
2022-07-19 02:05:01 +0430

    Add test id

--- src/components/Spaces/Space.tsx
+++ src/components/Spaces/Space.tsx
@@ -19,11 +19,17 @@ export default class Space extends React.Component<SpaceProps> {
         ) : (
           <img src={genericImage} alt="Generic" />
         )}
-        <label className="label name">{this.props.name}</label>
+        <label className="label name" data-testid="name-label">
+          {this.props.name}
+        </label>
         <br />
-        <label className="label id">{this.props.id}</label>
+        <label className="label id" data-testid="id-label">
+          {this.props.id}
+        </label>
         <br />
-        <label className="label location">{this.props.location}</label>
+        <label className="label location" data-testid="location-label">
+          {this.props.location}
+        </label>
         <br />
         <button onClick={(): void => this.props.reserveSpace(this.props.id)}>Reserve</button>
       </div>
